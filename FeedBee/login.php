9db0c1dc49641f1c4e7d93c48762054459f86f7e
Ayse Erdur <73952261+Ayse-Erdur@users.noreply.github.com>
2021-05-19 12:42:29 +0200

    Update login.php

--- FeedBee/login.php
+++ FeedBee/login.php
@@ -17,15 +17,6 @@ if($_POST){
 <!doctype html>
 <html lang="de">
     <head>
-        <!-- Meta Tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="style.css">
-        
-        
         <title>Anmelden</title>
     </head>
     <body>
