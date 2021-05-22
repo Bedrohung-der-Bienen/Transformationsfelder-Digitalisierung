c3f75ccb2889c858832577c4868b309a40dad1d1
babakmehrabipour <45423301+babakmehrabipour@users.noreply.github.com>
2021-05-22 22:59:13 +0200

    Update pflanze.php

--- FeedBee/pflanze.php
+++ FeedBee/pflanze.php
@@ -25,7 +25,7 @@
                         <ul class="nav nav-pills border-secondary mb-2" role="tablist">
                                 
                                 <li class="nav-item col">
-                                    <a class="nav-link active bg-light text-dark" id="beschreibung" data-toggle="pill" href="#beschreibung" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                                    <a class="nav-link active bg-light text-dark" data-toggle="pill" href="#beschreibung" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                                     </svg> Beschreibung</a>
                                 </li>
