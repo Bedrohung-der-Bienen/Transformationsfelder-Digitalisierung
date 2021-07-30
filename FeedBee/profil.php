e0ca28e6e7a35ae487347b39b57ab877446a73a6
mervetv <merve.tanriverdi@smail.th-koeln.de>
2021-07-30 17:21:08 +0200

    nav bar abmelden gefixt

--- FeedBee/profil.php
+++ FeedBee/profil.php
@@ -20,8 +20,9 @@
 		<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
 
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	
+		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="style.css">
     
               <!-- Optionales JavaScript -->
         <!-- jQuery, dann Popper.js, dann Bootstrap JS -->
@@ -43,11 +44,11 @@
 	        			<li class="nav-item"><a href="Pflanzenkatalog.php" class="nav-link"><img src="img/plant.png" alt="" style="height: 26px;width: 26px;"> Pflanzenkatalog</a></li>
 	        			<li class="nav-item "><a href="merkliste.php" class="nav-link"><img src="img/bookmark.png" alt="" style="height: 26px;width: 26px;"> Merkliste</a></li>
 						<li class="nav-item"><a href="Aussatkalender.php" class="nav-link"><img src="img/planner.png" alt="" style="height: 26px;width: 26px;"> Aussaatkalender</a></li>
-						<li class="nav-item dropdown ">
+						<li class="nav-item active dropdown ">
 						<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="profil.php" role="button" aria-haspopup="true" aria-expanded="false"><img src="img/profil.png" alt="" style="height: 26px;width: 26px;">Profil</a>
 							<div class="dropdown-menu">
-							<a class="dropdown-item" href="login.php"><img src="img/logout.png" alt="" style="height: 20px;width: 20px;"> Abmelden</a>
-						</li>
+							<a class="dropdown-item" href="login.php" style="min-width: inherit; display: inline-block;"><img src="img/logout.png" alt="" style="height: 20px;width: 20px;"> Abmelden</a></li>
+							</div>
 	        		</ul>
 	      		</div>
 			</div>
