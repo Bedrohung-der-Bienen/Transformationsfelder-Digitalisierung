7b69cb21f65fd9404209d41169f682fe7a36d4ce
Bouchra-Allachi <73944868+Bouchra-Allachi@users.noreply.github.com>
2021-10-08 13:26:51 +0200

    Merkliste

--- FeedBee/merkliste.php
+++ FeedBee/merkliste.php
@@ -22,7 +22,7 @@ if(isset($_SESSION['user'])) {
                 <div class="col-md">
                     <div class="card card-body mb-2">
                         <div class="media align-items-center align-items-lg-start text-center text-lg-left flex-column flex-lg-row">
-                            <div class="mr-2 mb-3 mb-lg-0"><img src="img/Thymian2.jpg" width="180" height="150" alt=""> </div>
+                            <div class="mr-2 mb-3 mb-lg-0"><img src="img/<?php print_r($row[7]);?>.jpg" width="180" height="150" alt=""> </div>
                             <h6 class="media-title font-weight-semibold mt-5" style="width: 170px;"> <a href="pflanze.php? id=<?php print_r($row[3]);?>" data-abc="true"><?php print_r($row[4]);?>
                             <a href="dropPflanze.php? id=<?php print_r($row[3]);?>" data-abc="true"><i class="material-icons" style="vertical-align: middle;">bookmark</i>
                             </a>
@@ -34,25 +34,25 @@ if(isset($_SESSION['user'])) {
                                         <div class="col-3">
                                             <div class="mt-3">
                                                 <img src="img/gießkanne.png" style="height: 30px; width: 30px;"/>
-                                                <p class="mb-0 text-muted"><?php print_r($row[15]);?></p>
+                                                <p class="mb-0 text-muted"><?php print_r($row[18]);?></p>
                                             </div>
                                         </div>
                                         <div class="col-3">
                                             <div class="mt-3">
                                                 <img src="img/sonne.png" style="height: 30px; width: 30px;"/>
-                                                <p class="mb-0 text-muted"><?php print_r($row[16]);?></p>
-                                            </div>
-                                        </div>
-                                        <div class="col-3">
-                                            <div class="mt-3">
-                                                <img src="img/bese.png" style="height: 30px; width: 30px;"/>
                                                 <p class="mb-0 text-muted"><?php print_r($row[19]);?></p>
                                             </div>
                                         </div>
                                         <div class="col-3">
                                             <div class="mt-3">
+                                                <img src="img/bese.png" style="height: 30px; width: 30px;"/>
+                                                <p class="mb-0 text-muted"><?php print_r($row[22]);?></p>
+                                            </div>
+                                        </div>
+                                        <div class="col-3">
+                                            <div class="mt-3">
                                                 <img src="img/kalender.png" style="height: 30px; width: 30px;"/>
-                                                <p class="mb-0 text-muted"><?php print_r($row[9]);?></p>
+                                                <p class="mb-0 text-muted"><?php print_r($row[12]);?></p>
                                             </div>
                                         </div>
                                     </div>
